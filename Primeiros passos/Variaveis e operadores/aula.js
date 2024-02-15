8eb757bcec0c07cef62d1f7774ae2c23d38a1f34
VinnyPC <vinisilvax3@gmail.com>
2024-02-15 11:52:22 -0300

    primeira aula

---
+++ Primeiros passos/Variaveis e operadores/aula.js
@@ -0,0 +1,16 @@
+// node aula.js
+console.log("Hello World");
+
+let variavel = 10;
+
+variavel = 20;
+
+const pi = 3.14;
+
+const precoCombustivel = 5.79;
+const mediaCombustivel = 10;
+const distancia = 100;
+
+const resultado = precoCombustivel * mediaCombustivel;
+
+console.log(resultado.toFixed(2));
